60e26cd2e9305d3ff3cbc569bedf0309c03b4f4b
Maxim Kott <maximkott@gmail.com>
2021-01-20 22:07:42 +0100

    Extract form binder

--- src/createFormBinder.ts
+++
@@ -1,4 +0,0 @@
-import { CreateFormBinder } from "./types"
-import { FormBinder } from "./FormBinder"
-
-export const createFormBinder: CreateFormBinder = (form) => new FormBinder(form)
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export * from "./types"
 export * from "./useForm"
-export * from "./FormBinder"
